928a795c8cf602c30419d7226e606b08ba09346e
Hanna <hania91414@gmail.com>
2018-06-05 21:45:04 +0200

    book contract updated

--- app/src/main/java/com/example/hanna/bookstoreappstage2/data/BookContract.java
+++ app/src/main/java/com/example/hanna/bookstoreappstage2/data/BookContract.java
@@ -1,12 +1,35 @@
 package com.example.hanna.bookstoreappstage2.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class BookContract {
 
     private BookContract(){}
 
+    public static final String CONTENT_AUTHORITY = "com.example.hanna.bookstoreappstage2";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_BOOKS = "books";
+
     public static final class BookEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the book data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of books.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single book.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
 
         public static final String _ID = BaseColumns._ID;
         public static final String TABLE_NAME = "books";
