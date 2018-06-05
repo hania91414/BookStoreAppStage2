beaa19812999821184d44ce807ca49fc8271c380
Hanna <hania91414@gmail.com>
2018-06-05 15:15:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hanna/bookstoreappstage2/MainActivity.java
@@ -0,0 +1,185 @@
+package com.example.hanna.bookstoreappstage2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.hanna.bookstoreappstage2.data.BookDbHelper;
+import com.example.hanna.bookstoreappstage2.data.BookContract.BookEntry;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button addDummyDataButton;
+
+    /** Database helper that will provide us access to the database */
+    private BookDbHelper mDbHelper;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProdDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        mDbHelper = new BookDbHelper(this);
+
+        addDummyDataButton = findViewById(R.id.button);
+        addDummyDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertBook();
+                displayDatabaseInfo();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the database.
+     */
+    private void displayDatabaseInfo() {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_PRODUCT_NAME,
+                BookEntry.COLUMN_PRICE,
+                BookEntry.COLUMN_QUANTITY,
+                BookEntry.COLUMN_SUPPLIER_NAME,
+                BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER};
+
+        // Perform a query on the books table
+        Cursor cursor = db.query(
+                BookEntry.TABLE_NAME,   // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,
+                null,                      // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
+
+
+        TextView displayView = findViewById(R.id.show_items_text_view);
+
+        try {
+            // Create a header in the Text View
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.setText("The book table contains " + cursor.getCount() + " books.\n\n");
+            displayView.append(BookEntry._ID + " - " +
+                    BookEntry.COLUMN_PRODUCT_NAME + " - " +
+                    BookEntry.COLUMN_PRICE + " - " +
+                    BookEntry.COLUMN_QUANTITY + " - " +
+                    BookEntry.COLUMN_SUPPLIER_NAME + " - " +
+                    BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER + "\n");
+
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_QUANTITY);
+            int supplierColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_SUPPLIER_NAME);
+            int phoneColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                int currentPrice = cursor.getInt(priceColumnIndex);
+                int currentQuantity = cursor.getInt(quantityColumnIndex);
+                String currentSupplier = cursor.getString(supplierColumnIndex);
+                int currentPhoneNumber = cursor.getInt(phoneColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(("\n" + currentID + " - " +
+                        currentName + " - " +
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentSupplier + " - " +
+                        currentPhoneNumber));
+            }
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+    }
+
+    /**
+     * Helper method to insert hardcoded data into the database. For debugging purposes only.
+     */
+    private void insertBook() {
+        // Gets the database in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object where column names are the keys,
+        // and attributes are the values.
+        ContentValues values = new ContentValues();
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, "Gone with the wind");
+        values.put(BookEntry.COLUMN_PRICE, 10);
+        values.put(BookEntry.COLUMN_QUANTITY, 2);
+        values.put(BookEntry.COLUMN_SUPPLIER_NAME, "Best Supplier");
+        values.put(BookEntry.COLUMN_SUPPLIER_PHONE_NUMBER, 76343535);
+
+        long newRowId = db.insert(BookEntry.TABLE_NAME, null, values);
+    }
+}
